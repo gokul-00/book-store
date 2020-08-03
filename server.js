6bc47f50a841a4b29d8fb3c42119b93d59506449
gokul-00 <ygokul1702@gmail.com>
2020-08-03 23:08:52 +0530

    initial author route

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
-
+//import dependencies
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose');
-
-const indecRouter = require('./routes/index');
-
+const bodyParser = require('body-parser');
+//import routes
+const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+//app init
 const app = express()
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false, limit : '10mb' }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+//mongoDB connection
 mongoose.connect(process.env.DATABASE_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -21,14 +30,18 @@ const db = mongoose.connection
 db.once('open',()=>console.log('connected to mongoDB...'))
 db.on('error',err => console.error(err))
 
+//view engine setup
 app.set('view engine','ejs')
 app.set('views' , __dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-app.use('/',indecRouter)
+//connecting routes
+app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 
+//connecting to server
 app.listen(process.env.PORT||3000,()=>{
     console.log('server running on port 3000...')
 })
